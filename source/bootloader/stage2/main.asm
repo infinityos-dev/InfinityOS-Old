95ac51fa41fc4d90ec2b31e1733eedb28bdb6044
The Douck <151237703+TheDouck@users.noreply.github.com>
2024-11-20 16:48:27 +0100

    sum stuff

--- source/bootloader/stage2/main.asm
+++ source/bootloader/stage2/main.asm
@@ -7,21 +7,6 @@ global entry
 
 entry:
     cli
-    gdtr    DW 0 ; For limit storage
-            DD 0 ; For base storage
-
-    setGdt:
-        XOR   EAX, EAX
-        MOV   AX, DS
-        SHL   EAX, 4
-        ADD   EAX, 'GDT'
-        MOV   [gdtr + 2], eax
-        MOV  EAX, 'GDT_end'
-        SUB   EAX, 'GDT'
-        MOV   [gdtr], AX
-        LGDT  [gdtr]
-        RET
-    
     ; setup stack
     mov ax, ds
     mov ss, ax
@@ -34,18 +19,6 @@ entry:
     push dx
     call _cstart_
 
-    cli            ; disable interrupts
-    lgdt [gdtr]    ; load GDT register with start address of Global Descriptor Table
-    mov eax, cr0 
-    or al, 1       ; set PE (Protection Enable) bit in CR0 (Control Register 0)
-    mov cr0, eax
-
-    ; Perform far jump to selector 08h (offset into GDT, pointing at a 32bit PM code segment descriptor) 
-    ; to load CS with proper PM32 descriptor)
-    jmp 08h:PModeMain
-
-    PModeMain:
-    ; load DS, ES, FS, GS, SS, ESP
 
     cli
     hlt
--- source/bootloader/stage2/main.c
+++ source/bootloader/stage2/main.c
@@ -1,28 +1,6 @@
 #include "stdint.h"
 #include "stdio.h"
 
-void encodeGdtEntry(uint8_t *target, struct GDT source)
-{
-    // Check the limit to make sure that it can be encoded
-    if (source.limit > 0xFFFFF) {kerror("GDT cannot encode limits larger than 0xFFFFF");}
-    
-    // Encode the limit
-    target[0] = source.limit & 0xFF;
-    target[1] = (source.limit >> 8) & 0xFF;
-    target[6] = (source.limit >> 16) & 0x0F;
-    
-    // Encode the base
-    target[2] = source.base & 0xFF;
-    target[3] = (source.base >> 8) & 0xFF;
-    target[4] = (source.base >> 16) & 0xFF;
-    target[7] = (source.base >> 24) & 0xFF;
-    
-    // Encode the access byte
-    target[5] = source.access_byte;
-    
-    // Encode the flags
-    target[6] |= (source.flags << 4);
-}
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
@@ -33,4 +11,5 @@ void _cdecl cstart_(uint16_t bootDrive)
     printf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\r\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
     printf("Formatted %ld %lx %lld %llx\r\n", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
     for (;;);
+    
 }
