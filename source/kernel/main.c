218ee3d8c74bec604c7bb620836a2b11a19d2c4c
Viktor Popp <viktorpopp.dev@gmail.com>
2024-11-27 16:17:30 +0100

    Did some formatting

--- source/kernel/main.c
+++ source/kernel/main.c
@@ -9,11 +9,8 @@ extern uint8_t __end;
 void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 {
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
-
     HAL_Initialize();
-
     clrscr();
-
     printf("Hello world from kernel!!!\n");
 
 end:
