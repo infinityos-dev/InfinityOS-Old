f6c40815e7c72f733b008047667fd7cda24eb9bc
Viktor Popp <viktorpopp.dev@gmail.com>
2024-12-12 08:19:48 +0100

    Scons does not work

--- scripts/setup_pacman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -S nasm qemu-system-x86 mtools dosfstools
-echo "***IMPORTANT, THEY ARE BOTH REQUIRED***"
-echo "bochsbios and bochs-x are available on https://github.com/bochs-emu/Bochs/tree/master"
-echo "vgabios is available on https://github.com/bochs-emu/VGABIOS"
